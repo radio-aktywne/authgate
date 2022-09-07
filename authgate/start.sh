f6d32561914e772455824a7f75cf9dec74dce254
Sebastian Pietras <spietras1998@gmail.com>
2022-09-07 03:35:38 +0200

    Enabled auth header forwarding

--- authgate/start.sh
+++ authgate/start.sh
@@ -4,6 +4,8 @@ cat <<EOF >./traefik.yml
 entryPoints:
   http:
     address: ":${AUTHGATE_PORT:-20000}"
+    forwardedHeaders:
+      trustedIPs: "${AUTHGATE_TRUSTED_IPS:-127.0.0.1}"
 
 api:
   dashboard: true
