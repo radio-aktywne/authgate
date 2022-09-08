4fd7e363a8bd8a4328ad858825aee01c84de2277
Sebastian Pietras <spietras1998@gmail.com>
2022-09-08 10:14:38 +0200

    Moved to HTTPS

--- authgate/start.sh
+++ authgate/start.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+until [ -f /etc/certs/backend/ca.pem ]; do
+  echo "Waiting for backend certificates..."
+  sleep 1
+done
+
+cat /etc/certs/backend/ca.pem >>/etc/ssl/certs/ca-certificates.crt
+
+until [ -f /etc/certs/frontend/cert.pem ] && [ -f /etc/certs/frontend/key.pem ]; do
+  echo "Waiting for frontend certificates..."
+  sleep 1
+done
+
 cat <<EOF >./traefik.yml
 entryPoints:
-  http:
+  https:
     address: ":${AUTHGATE_PORT:-20000}"
+    http:
+      tls: { }
     forwardedHeaders:
       trustedIPs: "${AUTHGATE_TRUSTED_IPS:-127.0.0.1}"
+
+serversTransport:
+  rootCAs:
+    - /etc/certs/backend/ca.pem
 
 api:
   dashboard: true
